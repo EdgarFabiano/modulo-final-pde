a4c5e8657cfb1b0aaf380c79994db1d40666e826
EdgarFabiano <eddy.led@gmail.com>
2019-08-05 16:00:14 -0300

    Corrigindo formatter

--- edgar/src/main/java/br/net/mirante/pde/util/format/TextFormatUtil.java
+++ edgar/src/main/java/br/net/mirante/pde/util/format/TextFormatUtil.java
@@ -14,12 +14,12 @@ public class TextFormatUtil {
 
     public static String formatCpf(String cpf) {
         return StringUtils.isNumeric(cpf) &&
-                cpf.length() == 11 ? cpf.replaceAll("(.{3})(.{3})(.{3})(.{2})", "$1.$2.$3-$4") : null;
+                cpf.length() == 11 ? cpf.replaceAll("(.{3})(.{3})(.{3})(.{2})", "$1.$2.$3-$4") : cpf;
     }
 
     public static String formatCnpj(String cnpj) {
         return StringUtils.isNumeric(cnpj) &&
-                cnpj.length() == 14 ? cnpj.replaceAll("(.{2})(.{3})(.{3})(.{4})(.{2})", "$1.$2.$3/$4-$5") : null;
+                cnpj.length() == 14 ? cnpj.replaceAll("(.{2})(.{3})(.{3})(.{4})(.{2})", "$1.$2.$3/$4-$5") : cnpj;
     }
 
     public static String formatCpfCnpj(String cpfCnpj) {
@@ -29,12 +29,12 @@ public class TextFormatUtil {
 
     public static String formatTelefone(String telefone) {
         return StringUtils.isNumeric(telefone) &&
-                (telefone.length() == 11 || telefone.length() == 10) ? telefone.replaceAll("(.{2})(.{4,5})(.{4})", "($1) $2-$3") : null;
+                (telefone.length() == 11 || telefone.length() == 10) ? telefone.replaceAll("(.{2})(.{4,5})(.{4})", "($1) $2-$3") : telefone;
     }
 
     public static String formatCep(String cep) {
         return StringUtils.isNumeric(cep) &&
-                cep.length() == 8 ? cep.replaceAll("(.{2})(.{3})(.{3})", "$1.$2-$3") : null;
+                cep.length() == 8 ? cep.replaceAll("(.{2})(.{3})(.{3})", "$1.$2-$3") : cep;
     }
 
 }
